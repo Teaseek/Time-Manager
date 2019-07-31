aef76719343e03ab6b6255cdd379afaf398679c0
Teaseek <lobakov.nek@gmail.com>
2019-07-31 16:33:53 +0500

    add viewmodels

--- TimeManager/Views/MainWindow.xaml.cs
+++ TimeManager/Views/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TimeManager.ViewModels;
 
 namespace TimeManager
 {
@@ -20,6 +21,7 @@ namespace TimeManager
 		public MainWindow()
 		{
 			InitializeComponent();
+			DataContext = new MainVM();
 		}
 	}
 }
