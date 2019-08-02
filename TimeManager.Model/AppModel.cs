fb4795e047fae981f62e64fcc1cbfec9af2cae1f
Teaseek <lobakov.nek@gmail.com>
2019-08-02 18:06:44 +0500

    add model

--- TimeManager.Model/AppModel.cs
+++ TimeManager.Model/AppModel.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace TimeManager.Model
 {
-	public class AppModel
+	public static class AppModel
 	{
+		public static List<Session> Sessions { get; } = new List<Session>();
+		public static List<Session> RemovedSessions { get; } = new List<Session>();
+		public static Session CurrentSession { get; set; }
+		public static Timer CurrentTimer => CurrentSession.Timer;
+
+		public static Session AddSession(Timer timer, string name)
+		{
+			var session = new Session(timer, name);
+			Sessions.Add(session);
+			return session;
+		}
+		public static void SetCurrentSession(Session session)
+		{
+			if (!Sessions.Contains(session))
+				Sessions.Add(session);
+			CurrentSession = session;
+		}
+		public static void RemoveSession(Session session)
+		{
+			RemovedSessions.Add(session);
+			Sessions.Remove(session);
+		}
+
 	}
 }
